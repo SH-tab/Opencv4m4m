00efc6c29e49655474f58708074710d95bf1c135
SHM-tab <shminn_10@hotmail.com>
2020-03-02 19:41:55 -0600

    Adding bluetooth comunication and a mask for line detection and Solving problems with mask and image RAM storage.

--- app/src/main/java/com/example/opencvappm4m/MainActivity.java
+++ app/src/main/java/com/example/opencvappm4m/MainActivity.java
@@ -7,190 +7,178 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.SurfaceView;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
-import org.opencv.android.OpenCVLoader;
-
-import org.opencv.core.Mat ;
-import org.opencv.core.Point ;
-import org.opencv.core.Scalar ;
-import org.opencv.imgproc.Imgproc ;
-
-
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+import java.util.Set;
 
 
 
-    CameraBridgeViewBase cameraBridgeViewBase ;
-    BaseLoaderCallback baseLoaderCallback ;
+public class MainActivity extends AppCompatActivity {
+
+
+    // Depuración de LOGCAT
+
+    private static final String TAG = "DispositivosBT";
+
+
+    // Declaracion de variables
+
+    private BluetoothAdapter mBtAdapter;
+
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+
+    ListView mLv_pairedDevices;
+    //Button mBtnConnect ;
+    TextView mTv_BtnConnect ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState) ;
-        setContentView(R.layout.activity_main) ;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
-        cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.cameraViewOpencv) ;
-        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE) ;
-        cameraBridgeViewBase.setCvCameraViewListener(this) ;
+    @Override
+    public void onResume() {
 
-        baseLoaderCallback = new BaseLoaderCallback(this) {
-            @Override
-            public void onManagerConnected(int status) {
-                super.onManagerConnected(status);
-                if(status == BaseLoaderCallback.SUCCESS) {
+        super.onResume();
 
-                    cameraBridgeViewBase.enableView() ;
 
-                } else {
+        VerificarEstadoBT() ;
 
-                    super.onManagerConnected(status) ;
+        CameraPermissions() ;
 
-                }
+
+        // Inicializa la array que contendra la lista de los dispositivos bluetooth vinculados
+
+        ArrayAdapter<String> mPairedDevicesArrayAdapter = new ArrayAdapter<>(this, R.layout.nombre_dispositivos);
+
+
+
+        // Vinculacion para ListView y Finds
+
+        mLv_pairedDevices = findViewById(R.id.Lv_pairedDevices);
+        mLv_pairedDevices.setAdapter(mPairedDevicesArrayAdapter);
+        mLv_pairedDevices.setOnItemClickListener(mDeviceClickListener);
+
+        //mBtnConnect = findViewById(R.id.BtnConnect) ;
+        mTv_BtnConnect = findViewById(R.id.Tv_BtnConnect) ;
+
+
+        // Obtiene el adaptador local Bluetooth adapter
+
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        Set <BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+
+        mPairedDevicesArrayAdapter.clear() ;
+
+
+        // Añade los dispositivos ya emparejados al array
+
+        if (pairedDevices.size() > 0) {
+
+            mTv_BtnConnect.setText(getText(R.string.Txt_Tv_BtnConnect));
+
+            for (BluetoothDevice device : pairedDevices) {
+                mPairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
             }
-        } ;
 
+        }  else {
 
-        int MY_PERMISSIONS_REQUEST_CAMERA=0;
+            mTv_BtnConnect.setText(getText(R.string.Txt_Tv_BtnConnect_no));
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-        {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA))
-            {
-
-                /* gg */
-            }
-            else
-            {
-                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA );
-
-            }
         }
 
     }
 
+    // Configura un (on-click) para la lista
+
+    private AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
+
+        public void onItemClick(AdapterView av, View v, int arg2, long arg3) {
+
+            // Obtener la dirección MAC del dispositivo, que son los últimos 17 caracteres en la vista
+
+            String info = ((TextView) v).getText().toString();
+            String address = info.substring(info.length() - 17);
 
 
-    @Override
-    protected void onResume() {
-        super.onResume();
 
-        if(!OpenCVLoader.initDebug()) {
+            // Realiza un intent para iniciar la siguiente actividad
+            // mientras toma un EXTRA_DEVICE_ADDRESS que es la dirección MAC.
 
-            Toast.makeText(this, "There is a problem with OpenCV service", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(MainActivity.this, ArtificialVision.class);
+            i.putExtra(EXTRA_DEVICE_ADDRESS, address);
+            startActivity(i);
+
+        }
+    };
+
+
+
+    // -------------------------  CALLED FUNCTIONS  ------------------------------------------------
+
+    private void VerificarEstadoBT() {
+
+        // Comprueba que el dispositivo tiene Bluetooth y que está encendido.
+
+        mBtAdapter= BluetoothAdapter.getDefaultAdapter();
+
+        if(mBtAdapter==null) {
+
+            Toast.makeText(getBaseContext(), "El dispositivo no soporta Bluetooth", Toast.LENGTH_SHORT).show();
 
         } else {
 
-            baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
+            if (mBtAdapter.isEnabled()) {
 
+                Log.d(TAG, "...Bluetooth Activado...");
+
+            } else {
+
+                //Solicita al usuario que active Bluetooth
+
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+
+            }
         }
-
     }
 
 
+    private void CameraPermissions() {
 
-    @Override
-    protected void onPause() {
-        super.onPause();
+        int MY_PERMISSIONS_REQUEST_CAMERA=0;
 
-        if(cameraBridgeViewBase!=null) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
 
-            cameraBridgeViewBase.disableView() ;
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
 
-        }
+                /* gg */
 
-    }
+            } else {
 
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA );
 
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        if(cameraBridgeViewBase!=null) {
-
-            cameraBridgeViewBase.disableView() ;
+            }
 
         }
-
-    }
-
-
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-
-    }
-
-
-
-    @Override
-    public void onCameraViewStopped() {
-
-    }
-
-
-
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-
-
-        // Putting de frame into a variable.
-        Mat src = inputFrame.gray() ;
-        Mat showFrame = inputFrame.rgba() ;
-
-        // Variables for define changes in image's pixels.
-        double alpha = 100 ;
-        double beta = 80 ;
-
-
-        // Here we aply Canny function to the frame.
-        // Parentheses content (from where, to where, alpha value, beta value).
-        Imgproc.Canny(src, src, alpha, beta) ;
-
-
-        /*
-        // Standard Hough Line Transform
-        Mat lines = new Mat(); // will hold the results of the detection
-        Imgproc.HoughLines(src, lines, 1, Math.PI/180, 400); // runs the actual detection
-
-        // Draw the lines
-        for (int x = 0; x < lines.rows(); x++) {
-            double rho = lines.get(x, 0)[0],
-                    theta = lines.get(x, 0)[1];
-            double a = Math.cos(theta), b = Math.sin(theta);
-            double x0 = a*rho, y0 = b*rho;
-            Point pt1 = new Point(Math.round(x0 + 1000*(-b)), Math.round(y0 + 1000*(a)));
-            Point pt2 = new Point(Math.round(x0 - 1000*(-b)), Math.round(y0 - 1000*(a)));
-            Imgproc.line( src, pt1, pt2, new Scalar(0, 0, 255), 3, Imgproc.LINE_AA, 0);
-        }
-        */
-
-
-        // Probabilistic Line Transform
-        Mat linesP = new Mat(); // will hold the results of the detection
-
-        Imgproc.HoughLinesP(src, linesP, 1, Math.PI/180, 200, 150, 10); // runs the actual detection
-
-        // Draw the lines
-        for (int x = 0; x < linesP.rows(); x++) {
-
-            double[] l = linesP.get(x, 0) ;
-            // Show the lines on inputFrame RGB variable.
-            Imgproc.line(showFrame, new Point(l[0], l[1]), new Point(l[2], l[3]), new Scalar(0, 0, 255), 3, Imgproc.LINE_AA, 0) ;
-
-        }
-
-
-        // Show results.
-            return showFrame ;
 
     }
 
 
 
 }
+
